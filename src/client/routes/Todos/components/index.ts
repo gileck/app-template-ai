0d2b875646b717eb5b542e8773f82c0881125d7a
gileck <gileck@wix.com>
2026-01-23 11:24:01 +0200

    feat: enhance todo list capabilites with more features
    Closes #33

--- src/client/routes/Todos/components/index.ts
+++ src/client/routes/Todos/components/index.ts
@@ -5,3 +5,4 @@
 export { CreateTodoForm } from './CreateTodoForm';
 export { TodoItem } from './TodoItem';
 export { DeleteTodoDialog } from './DeleteTodoDialog';
+export { TodoControls } from './TodoControls';
